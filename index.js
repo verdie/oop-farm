25ade7fe596bc1d4cd0033e5617cb8599607b7e4
halynaburdiian <verdamtion@gmail.com>
2019-07-10 15:40:43 +0200

    Finishing farm impl

--- index.js
+++ index.js
@@ -9,21 +9,17 @@ class Farm {
         this.crops.push(crop)
     }
 
-    calculateIncome(crop) {
+    calculateIncome(crop, animal) {
         return this.crops
         .map(crop => crop.getYieldInEuros())
-        .reduce((a,b) => a + b, 0);
+        .reduce((a,b) => a + b, 0)|| this.animals
+        .map(animal => animal.getValueInEuros())
+        .reduce((a,b) => a + b, 0)
     }
     
     addAnimal(animal){
         this.animals.push(animal)
 
-    }
-
-    calculateIncome(animal) {
-        return this.animals
-        .map(animal => animal.getValueInEuros())
-        .reduce((a,b) => a + b, 0)
     }
   }
 
